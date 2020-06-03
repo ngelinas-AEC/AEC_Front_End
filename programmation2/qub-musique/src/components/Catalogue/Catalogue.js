d2093f71bf179a7b0964be3145b1bb22f211ca58
ngelinas-AEC <wingelinas@gmail.com>
2020-06-03 10:09:51 -0400

    Create qub-musique

---
+++ programmation2/qub-musique/src/components/Catalogue/Catalogue.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Categorie} from '../Categorie/Categorie';
+
+export class Catalogue extends React.Component{
+  render() {
+    return (
+      <>
+      <Categorie nom="Albums"/>
+      </>
+      )
+  }
+}
