b0b94b84dc3831fb9d8c77baf390a251508a4f77
ngelinas-AEC <wingelinas@gmail.com>
2020-06-02 21:12:33 -0400

    Update QUB-Musique

---
+++ programmation2/qub-music/src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import {Accueil} from '../Accueil/Accueil'
+
+
+function App() {
+  return (
+        <Accueil/>
+  );
+}
+
+export default App;
---
+++ programmation2/qub-music/src/components/Deconnexion/Deconnexion.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+ 
+
+export class Deconnexion extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+        this.handleDeconexion = this.handleDeconexion.bind(this);
+    }
+
+    handleDeconexion(){
+        this.props.onClick();
+    }
+
+    render() {
+        return (
+                <div className="d-flex justify-content-end">
+                    <button onClick={this.handleDeconexion} type="" className="btn btn-danger " >Déconnexion</button>
+                </div>
+        );
+    }
+}
+
