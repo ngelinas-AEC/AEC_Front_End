f8b3d6e67cf66ee280ab98fcfe007c419862e350
ngelinas-AEC <wingelinas@gmail.com>
2020-06-09 18:01:59 -0400

    update qub-musique

---
+++ programmation2/qub-musique/build/precache-manifest.32f984749dac22a7ca102aecb7b42404.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b2383bc22a40950ceea0e5bfeb8996",
+    "url": "./index.html"
+  },
+  {
+    "revision": "93095abd46ea0f5a0438",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "869ff2d5b66c732b01b1",
+    "url": "./static/css/main.27f190fb.chunk.css"
+  },
+  {
+    "revision": "93095abd46ea0f5a0438",
+    "url": "./static/js/2.f36dc9fd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.f36dc9fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "869ff2d5b66c732b01b1",
+    "url": "./static/js/main.0976c74a.chunk.js"
+  },
+  {
+    "revision": "4af7e81e9bfd30c9c8fe",
+    "url": "./static/js/runtime-main.25374d25.js"
+  },
+  {
+    "revision": "dd7423693aa1833a051dc1f83f9317f6",
+    "url": "./static/media/back.dd742369.png"
+  },
+  {
+    "revision": "6ff7905435c4cd906fe8d0a735862c96",
+    "url": "./static/media/bisonnette1.6ff79054.jpg"
+  },
+  {
+    "revision": "e690ace872316eb7222cbbe7d40c29bc",
+    "url": "./static/media/bisonnette2.e690ace8.jpg"
+  },
+  {
+    "revision": "61407f26453e91a41b42ef45c9e4c372",
+    "url": "./static/media/bisonnette3.61407f26.jpg"
+  },
+  {
+    "revision": "6b7d5f8d521f664f2bd6897ce4ee4b0d",
+    "url": "./static/media/ciccone1.6b7d5f8d.jpg"
+  },
+  {
+    "revision": "d718f048830d7f293b83bf0a7f999158",
+    "url": "./static/media/ciccone2.d718f048.jpg"
+  },
+  {
+    "revision": "d83f724771981da1dbe835f419baa7fe",
+    "url": "./static/media/ciccone4.d83f7247.jpg"
+  },
+  {
+    "revision": "c6606524bbec4b1d4162bc702b91ecd9",
+    "url": "./static/media/qub-musique.c6606524.png"
+  }
+]);
