ca38196aaa1a76e524a1c795f7216773e581ef3d
ngelinas-AEC <wingelinas@gmail.com>
2020-08-21 16:22:41 -0400

    rwa v2

--- Integration2/PointNClick2/service-worker.js
+++ Integration2/PointNClick2/service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v5';
+const CACHE_NAME = 'static-cache-v6';
 
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
@@ -55,10 +55,8 @@ self.addEventListener('fetch', (evt) => {
     evt.respondWith(
         fetch(evt.request)
             .catch(() => {
-                return caches.open(CACHE_NAME)
-                    .then((cache) => {
-                        return cache.match('/Cochenille/PointNClick2/offline.html' );
-                    });
+                return caches.open(CACHE_NAME);
+
             })
     );
 });
