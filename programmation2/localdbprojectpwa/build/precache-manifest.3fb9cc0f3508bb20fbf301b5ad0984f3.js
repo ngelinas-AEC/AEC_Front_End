67f2fef930cd96949cdf33519325c3189a6d972f
ngelinas-AEC <wingelinas@gmail.com>
2020-08-25 19:40:40 -0400

    upd pwa

---
+++ programmation2/localdbprojectpwa/build/precache-manifest.3fb9cc0f3508bb20fbf301b5ad0984f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e656045694db5c35d457bd39969d6fd4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "753be43dfa3d3bfa8295",
+    "url": "./static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "7b0ee7632d360cb8e7a1",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "753be43dfa3d3bfa8295",
+    "url": "./static/js/2.7419cd5d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.7419cd5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b0ee7632d360cb8e7a1",
+    "url": "./static/js/main.39e40f79.chunk.js"
+  },
+  {
+    "revision": "bb4922d3edace0c405c7",
+    "url": "./static/js/runtime-main.11fb3d3b.js"
+  },
+  {
+    "revision": "3abe438f0c273281baa9da6fa9fcc7d2",
+    "url": "./static/media/pwa.3abe438f.png"
+  }
+]);
