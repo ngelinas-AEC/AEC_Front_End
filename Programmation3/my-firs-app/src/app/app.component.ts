4b2a1306f058f8b1f961250cc3ab22ae24be42e5
ngelinas-AEC <wingelinas@gmail.com>
2020-10-06 20:09:20 -0400

    First Update

---
+++ Programmation3/my-firs-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  titrePrincipal = 'ANGULAR';
+  sousTitre = 'Test1';
+  description = 'Première application Angular pour le cours Programmation WEB3';
+
+}
